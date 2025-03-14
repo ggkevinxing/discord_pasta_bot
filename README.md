db2a39847875708477b81d51a4bd6f8033e67ffc
Kevin Xing <kevinzhangxing@gmail.com>
2025-03-14 15:30:08 -0700

    Update to use new Discord APIs (#10)

--- README.md
+++ README.md
@@ -8,9 +8,9 @@ a discord bot that uses mongodb to make !commands for text/emoji binds
 
 1. [Create a new Discord App](https://discordapp.com/developers/applications/me)
 
-2. `pip install -r requirements.txt` to install python packages such as [discord.py](https://github.com/Rapptz/discord.py) and [pymongo](https://api.mongodb.com/python/current/installation.html). Also `dnspython` is also included if you plan on using cloud DB solutions such as MongoDB Atlas. 
+2. `pip install -r requirements.txt` to install python packages such as [discord.py](https://github.com/Rapptz/discord.py) and [pymongo](https://api.mongodb.com/python/current/installation.html). Also `dnspython` is also included if you plan on using cloud DB solutions such as MongoDB Atlas.
 
-3. Set your environment variables for `BOT_TOKEN` (found in the Discord Developers Dashboard under <Application> > Bots section) and `DATABASE_URI` at the very least; you must replace the bot token and mongodb uri, and the rest can be optionally changed as you see fit. You can see what is used as environment variables in `main.py`
+3. Set your environment variables for `BOT_TOKEN` (found in the Discord Developers Dashboard under <Application> > Bots section) and `DATABASE_URI` at the very least; you must replace the bot token and mongodb uri, and the rest can be optionally changed as you see fit. You can see what is used as environment variables in `main.py`. If you don't want to host a MongoDB instance locally, you can use [MongoDB Atlas Free tier](https://www.mongodb.com/cloud/atlas/register?expVariant=v1&isNewUser=true) and set up a cluster in the cloud for free!
 
 4. Add your bot to the desired Discord server(s) using `https://discordapp.com/oauth2/authorize?client_id=<CLIENT_ID>&scope=bot`, where <CLIENT_ID> is found from the Discord Developers dashboard
 
@@ -18,16 +18,19 @@ a discord bot that uses mongodb to make !commands for text/emoji binds
 
 # Default Commands
 
-- `!add <command> <pasta>`                
+- `!add <command> <pasta>`
+
   - creates !<command> bind such that <pasta> is posted to the channel every time a user posts !<command>
   - only administrators may add commands, but everyone can use the !<command> afterwards
   - if <command> already exists, it will be replaced with the newest <pasta>
 
-- `!remove <command>`                
+- `!remove <command>`
+
   - removes !<command> bind from database
   - only administrators may remove commands
 
-- `!help` or `!commands`                
+- `!help` or `!commands`
+
   - finds all possible custom commands for the given server and posts them in the channel where the command was used
 
 - there's also an easter egg command that you probably never want to use or experience the consequences of
--- main.py
+++ main.py
@@ -2,230 +2,303 @@ import time
 import os
 import sys
 import discord
+from discord.ext import commands
 import asyncio
 import logging
 
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger("bot")
+
+# Load environment variables
 load_dotenv()
 
-# TO-DO
-# Implement !changegame, !changenick
-# Clean up or modularize code
-# Format messages to look better on Discord
-# Format output to look better on command line
-
-# getting env variables
-token = os.environ.get("BOT_TOKEN")
-db_uri = os.environ.get("DATABASE_URI")
-nickname = os.environ.get("BOT_NICKNAME")
-game = os.environ.get("BOT_GAME")
+# Environment configuration
+TOKEN = os.environ.get("BOT_TOKEN")
+DB_URI = os.environ.get("DATABASE_URI")
+NICKNAME = os.environ.get("BOT_NICKNAME")
+GAME = os.environ.get("BOT_GAME")
 
 MAX_MESSAGE_LEN = 2000
 
-gOffCooldown = True
+# Setup intents (required in newer Discord API versions)
+intents = discord.Intents.default()
+intents.message_content = True  # Needed to read message content
+intents.members = True  # Needed for nickname changes
 
+# Initialize bot with command prefix
+bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
 
-# discord and database init
-bot = discord.Client()
-if db_uri:
-	print("CONNECTED TO DB")
-	db_client = MongoClient(db_uri)
-	db = db_client['Morton']
+# Database connection
+if DB_URI:
+    logger.info("Connecting to database")
+    db_client = MongoClient(DB_URI)
+    db = db_client['Morton']
 else:
-	print("ERROR: Database not set")
-	sys.exit()
+    logger.error("ERROR: Database URI not set")
+    sys.exit(1)
 
-#######################
-## changing presence ##
-#######################
+# Command cooldowns
+cooldowns = {}
+
+class CustomHelpCommand(commands.HelpCommand):
+    """Custom help command that lists all custom commands from the database"""
+    
+    async def send_bot_help(self, mapping):
+        ctx = self.context
+        await get_cmds(ctx)
+
+# Setup events
 @bot.event
 async def on_ready():
-	print("Logged in as: ", bot.user)
-	if nickname:
-		print("Nickname: " + nickname)
-		for member in bot.get_all_members():
-			if member.id == bot.user.id:
-				await bot.change_nickname(member, nickname)
-	if game:
-		print("Playing: " + game)
-		await bot.change_presence(game=discord.Game(type=0, name=game))	# why type=0 has to be specified here to work, i have no idea
-	print("------")
+    """Called when the bot is ready and connected to Discord"""
+    logger.info(f"Logged in as: {bot.user} (ID: {bot.user.id})")
+    
+    # Set initial activity
+    if GAME:
+        logger.info(f"Setting activity to: {GAME}")
+        await bot.change_presence(activity=discord.Game(name=GAME))
+        
+    logger.info("Bot is ready!")
 
 @bot.event
-async def on_server_join(server):
-	print("Joined " + server.name + " as: " + bot.user.name + " | " + bot.user.id)
-	if nickname:
-		print("Nickname: " + nickname)
-		for member in bot.get_all_members():
-			if member.id == bot.user.id:
-				await bot.change_nickname(member, nickname)
-	if game:
-		print("Playing: " + game)
-		await bot.change_presence(game=discord.Game(type=0, name=game))
-	print("------")
+async def on_guild_join(guild):
+    """Called when the bot joins a new guild (server)"""
+    logger.info(f"Joined new guild: {guild.name} (ID: {guild.id})")
+    
+    # Set nickname if specified
+    if NICKNAME:
+        try:
+            me = guild.me
+            await me.edit(nick=NICKNAME)
+            logger.info(f"Changed nickname to {NICKNAME} in {guild.name}")
+        except discord.Forbidden:
+            logger.warning(f"Could not change nickname in {guild.name}: Missing permissions")
+        except Exception as e:
+            logger.error(f"Error changing nickname: {e}")
 
-##################################
-## database commands ##
-##################################
-async def add_cmd(message):
-	tmpArr = message.content.split()
-	# checking that it's at least 3 words long (roughly in the form of !add <command> <pasta>)
-	if len(tmpArr) > 2:
-		command = tmpArr[1]
-		prefix = "!add " + command
-		if command.startswith("!"):
-			command = command[1:]
-		# checking if command is valid, i.e. contains characters after "!" and does not use special characters, isn't a default command
-		if (command == "add" or command == "help" or command == "commands"):
-			await message.channel.send("ERROR: Cannot override hardcoded commands.")
-		elif command.strip() and command.isalnum():
-			pasta = message.content[len(prefix):].strip()
-			# have to make sure it's not a command in itself trying to mess with things
-			if pasta.startswith("!") == False and message.guild is not None:
-				collection = db[str(message.guild.id)] # keeping a collection for each server
-				writeDocument = {}
-				writeDocument['_id'] = command
-				writeDocument['content'] = pasta
-				result = collection.update_one({'_id': writeDocument['_id']}, {'$set': writeDocument}, upsert=True)
-				
-				# check if add or replace
-				if result.upserted_id is not None:
-					await message.channel.send("SUCCESS: Command '!" + command + "' has been added")
-				else:
-					await message.channel.send("SUCCESS: Command '!" + command + "' has been replaced")
-		else:
-			await message.channel.send("ERROR: User " + message.author.display_name + " has provided invalid command '" + command + "' to add.\n----------\n" +
-				"Please ensure that no special symbols are used in the command you want to add.\n" +
-				"Example: !add infinity In time you will know what it's like to lose. To feel so desperately that you're right, yet to fail all the same. Dread it. Run from it. Destiny still arrives.")
+@bot.event
+async def on_command_error(ctx, error):
+    """Global error handler for command errors"""
+    if isinstance(error, commands.CommandNotFound):
+        command = ctx.message.content[1:]  # Remove the prefix
+        collection = db[str(ctx.guild.id)]
+        pasta_document = collection.find_one({'_id': command})
+        
+        if pasta_document is not None:
+            await ctx.send(pasta_document['content'])
+        else:
+            await ctx.send("ERROR: Message starts with '!' but I don't recognize this command. Use !help or !commands to see what's available.")
+    elif isinstance(error, commands.MissingPermissions):
+        await ctx.send(f"ERROR: User {ctx.author.display_name} has insufficient permissions to use command.")
+    else:
+        logger.error(f"Command error: {error}")
 
-async def remove_cmd(message):
-	tmpArr = message.content.split()
-	if len(tmpArr) != 2:
-		await message.channel.send("ERROR: !remove failed - Invalid input format. Use !remove <command>, where <command> is one word.")
-	else:
-		command = tmpArr[1]
-		if command.startswith("!") == True:
-			command = command[1:]
-		collection = db[str(message.guild.id)]
-		result = collection.delete_one({'_id': command})
-		if result.deleted_count == 0:
-			await message.channel.send("ERROR: Could not remove, Command '!" + command + "' not found. On the bright side, you wanted to remove it anyways, right?")
-		else:
-			await message.channel.send("SUCCESS: Command '!" + command + "' has been removed")
+# Admin commands
+@bot.command(name="add")
+@commands.has_permissions(administrator=True)
+async def add_cmd(ctx, command: str = None, *, pasta: str = None):
+    """Add a custom command to the bot"""
+    if not command or not pasta:
+        await ctx.send("ERROR: Invalid format. Use !add <command> <response text>")
+        return
+        
+    # Remove ! if it's included in the command
+    if command.startswith("!"):
+        command = command[1:]
+        
+    # Check if command is valid
+    if command in ("add", "help", "commands", "remove", "changegame", "changenick"):
+        await ctx.send("ERROR: Cannot override hardcoded commands.")
+        return
+        
+    if not command.strip() or not command.isalnum():
+        await ctx.send("ERROR: Command must be alphanumeric with no spaces.")
+        return
+        
+    # Check that pasta doesn't start with !
+    if pasta.startswith("!"):
+        await ctx.send("ERROR: Command response cannot start with !")
+        return
+        
+    # Add command to database
+    collection = db[str(ctx.guild.id)]
+    write_document = {'_id': command, 'content': pasta}
+    result = collection.update_one({'_id': command}, {'$set': write_document}, upsert=True)
+    
+    # Check if command was added or replaced
+    if result.upserted_id is not None:
+        await ctx.send(f"SUCCESS: Command '!{command}' has been added")
+    else:
+        await ctx.send(f"SUCCESS: Command '!{command}' has been replaced")
 
-async def get_cmds(message):
-	header = message.guild.name + " commands:"
-	initMsg = await message.channel.send(header)
-	collection = db[str(message.guild.id)] # theoretically, we weren't passed a private message so we should have message.guild.id
-	commands = collection.find().batch_size(10).sort('_id',1)
-	temp = ""
+@bot.command(name="remove")
+@commands.has_permissions(administrator=True)
+async def remove_cmd(ctx, command: str = None):
+    """Remove a custom command from the bot"""
+    if not command:
+        await ctx.send("ERROR: !remove failed - Invalid input format. Use !remove <command>")
+        return
+        
+    # Remove ! if it's included in the command
+    if command.startswith("!"):
+        command = command[1:]
+        
+    # Remove command from database
+    collection = db[str(ctx.guild.id)]
+    result = collection.delete_one({'_id': command})
+    
+    if result.deleted_count == 0:
+        await ctx.send(f"ERROR: Could not remove, Command '!{command}' not found. On the bright side, you wanted to remove it anyways, right?")
+    else:
+        await ctx.send(f"SUCCESS: Command '!{command}' has been removed")
 
-	# redundant code as seen on post_txt, welp
-	for command in commands:
-		line = "!" + command['_id'] + "\n"
-		if len(temp + line) > MAX_MESSAGE_LEN:
-			await message.channel.send(temp)
-			temp = ""
-		temp += line
-	if temp:
-		await message.channel.send(temp)
+@bot.command(name="changegame")
+@commands.has_permissions(administrator=True)
+async def change_game(ctx, *, game: str = None):
+    """Change the bot's playing status"""
+    if not game:
+        await ctx.send("ERROR: Please provide a game name")
+        return
+        
+    await bot.change_presence(activity=discord.Game(name=game))
+    await ctx.send(f"SUCCESS: Changed playing status to '{game}'")
 
+@bot.command(name="changenick")
+@commands.has_permissions(administrator=True)
+async def change_nickname(ctx, *, nickname: str = None):
+    """Change the bot's nickname on the server"""
+    if not nickname:
+        await ctx.send("ERROR: Please provide a nickname")
+        return
+        
+    try:
+        await ctx.guild.me.edit(nick=nickname)
+        await ctx.send(f"SUCCESS: Changed nickname to '{nickname}'")
+    except discord.Forbidden:
+        await ctx.send("ERROR: I don't have permission to change my nickname")
+    except Exception as e:
+        await ctx.send(f"ERROR: Could not change nickname: {e}")
 
-####################
-## misc functions ##
-####################
+@bot.command(name="commands", aliases=["help"])
+async def get_cmds(ctx):
+    """List all available commands"""
+    header = f"{ctx.guild.name} commands:"
+    init_msg = await ctx.send(header)
+    
+    # Get built-in commands
+    built_in_commands = "Built-in commands:\n"
+    built_in_commands += "!add <command> <response> - Add a new command\n"
+    built_in_commands += "!remove <command> - Remove a command\n"
+    built_in_commands += "!changegame <game> - Change bot's playing status\n"
+    built_in_commands += "!changenick <nickname> - Change bot's nickname\n"
+    built_in_commands += "!commands or !help - Show this help message\n\n"
+    built_in_commands += "Custom commands:"
+    
+    await ctx.send(built_in_commands)
+    
+    # Get custom commands from database
+    collection = db[str(ctx.guild.id)]
+    commands = collection.find().batch_size(10).sort('_id', 1)
+    temp = ""
 
-# posts textfilename.txt into given channel
-async def post_txt(textfilename, channel):
-	global gOffCooldown
-	gOffCooldown = False
-	print("post_txt(" + textfilename + ")")
-	file = textfilename + ".txt"
-	message = ""
-	with open(file, "rb") as f:
-		for l in f:
-			line = l.decode(errors='ignore')
-			# don't want to accidentally cross discord's limit of 2000, post message if line with message exceeds 2000 chars
-			if len(message + line) > MAX_MESSAGE_LEN:
-				await channel.send(message)
-				message = ""
-			if line.strip():
-				message += line
-	if message:
-		await channel.send(message)
-	await asyncio.sleep(300) # arbitrary 5-min cooldown timer
-	gOffCooldown = True
+    for command in commands:
+        line = f"!{command['_id']}\n"
+        if len(temp + line) > MAX_MESSAGE_LEN:
+            await ctx.send(temp)
+            temp = ""
+        temp += line
+        
+    if temp:
+        await ctx.send(temp)
 
-################
-## on_message ##
-################
+# Helper function for easter eggs
+async def post_txt(textfilename, user):
+    """Post the contents of a text file to the channel"""
+    global cooldowns
+    
+    # Set cooldown
+    cooldowns[textfilename] = True
+    
+    file = f"{textfilename}.txt"
+    message = ""
+    
+    try:
+        with open(file, "rb") as f:
+            for l in f:
+                line = l.decode(errors='ignore')
+                # Respect Discord's message length limit
+                if len(message + line) > MAX_MESSAGE_LEN:
+                    await user.send(message)
+                    message = ""
+                if line.strip():
+                    message += line
+        
+        if message:
+            await user.send(message)
+            
+    except FileNotFoundError:
+        logger.error(f"Text file not found: {file}")
+    except Exception as e:
+        logger.error(f"Error posting text file: {e}")
+    
+    # Reset cooldown after 5 minutes
+    await asyncio.sleep(300)
+    cooldowns[textfilename] = False
+
+# Easter egg listener
 @bot.event
 async def on_message(message):
-	global gOffCooldown
+    """Process messages for easter eggs and command handling"""
+    # Ignore messages from bots and DMs
+    if message.author.bot or message.guild is None:
+        if message.author != bot.user and message.guild is None:
+            await message.channel.send("ERROR: I don't currently have support for any commands in private messages. Sorry!")
+        return
+    
+    # Log messages
+    logger.info(f"{message.guild.name} | {message.channel.name} | {message.author.name}: {message.content}")
+    
+    # Easter eggs
+    infinity_triggers = [
+        "In time you will know what it's like to lose.",
+        "In Time",
+        "Destiny still arrives.",
+        "Fun isn't something one considers from balancing the universe.",
+        "In",
+        "Fun"
+    ]
+    
+    if any(message.content.startswith(trigger) for trigger in infinity_triggers):
+        if not cooldowns.get("avengers-iw", False):
+            await post_txt("avengers-iw", message.author)
+        else:
+            await message.channel.send("Anti-Avengers Initiative is on cooldown. I'm probably still posting it to someone right now. Enjoy your freedom while you can!")
+    
+    # Process commands
+    await bot.process_commands(message)
 
-	# TODO: check if starts with ! then switch statement on the word that follows the ! instead of all these ifs and repeated startswith stuff?
+def run_bot():
+    """Run the bot with auto-restart on errors"""
+    while True:
+        try:
+            bot.run(TOKEN)
+        except discord.errors.LoginFailure:
+            logger.critical("Invalid token. Please check your BOT_TOKEN environment variable.")
+            sys.exit(1)
+        except Exception as e:
+            logger.error(f"Error: {e}")
+            logger.info("Restarting in 2 minutes...")
+            time.sleep(120)
+        except KeyboardInterrupt:
+            logger.info("Keyboard interrupt received. Shutting down.")
+            sys.exit(0)
 
-	if message.author != bot.user and message.guild is None:
-		await message.channel.send("ERROR: I don't currently have support for any commands in private messages. Sorry!")
-
-	elif message.author.bot == False:
-		print(message.guild.name + " | " + message.channel.name + " | " + message.author.name + ": " + message.content)
-
-		# explicit commands ("!" prefix)
-		# use double quotes to avoid escape characters on apostrophes PLEASE
-		if message.content.startswith("!add ") or message.content.startswith("!remove "):
-			if message.channel.permissions_for(message.author).administrator == True:
-				if message.content.startswith("!add"):
-					await add_cmd(message)
-				else:	# perhaps this will become an elif if another admin permission needing command is made
-					await remove_cmd(message)
-			else:
-				await message.channel.send("ERROR: User " + message.author.display_name + " has insufficient permissions to use command.")
-
-		elif message.content.startswith("!help") or message.content.startswith("!commands"):
-			await get_cmds(message)
-
-		elif message.content.startswith("!") and len(message.content) > 1:
-			# time to check if we have a database set and if it was a valid command
-			command = message.content[1:]
-			collection = db[str(message.guild.id)]
-			pastaDocument = collection.find_one({'_id': command})
-			if pastaDocument is not None:
-				await message.channel.send(pastaDocument['content'])
-			else:
-				await message.channel.send("ERROR: Message starts with '!' but I don't recognize this command. Use !help or !commands to see what's available.")
-
-		# trigger commands, aka easter eggs
-
-		elif (message.content.startswith("In time you will know what it's like to lose.") or
-		message.content.startswith("In Time") or
-		message.content.startswith("Destiny still arrives.") or
-		message.content.startswith("Fun isn't something one considers from balancing the universe.") or
-		message.content == "In" or message.content == "Fun"):
-			if gOffCooldown == True:
-				await post_txt("avengers-iw", message.author)
-			else:
-				await message.channel.send("Anti-Avengers Initiative is on cooldown. I'm probably still posting it to someone right now. Enjoy your freedom while you can!")
-
-def bot_run(client, *args, **kwargs):
-	# http://discordpy.readthedocs.io/en/latest/api.html#discord.Client.run
-	# like Client.run except it restarts instead of closes the event loop on exception
-	loop = asyncio.get_event_loop()
-	while(True):
-		try:
-			loop.run_until_complete(client.start(*args, **kwargs))
-		except Exception as e:
-			print("Error", e)  # or use proper logging
-			print("Waiting until restart")
-			time.sleep(120)
-
-while(True):
-	try:
-		bot_run(bot, token)
-	except KeyboardInterrupt as e:
-		print("KEYBOARD INTERRUPT, EXITING")
-		sys.exit()
-	except Exception as e:
-		continue
+if __name__ == "__main__":
+    run_bot()
